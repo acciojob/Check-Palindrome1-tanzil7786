8887fcf3ec79b1dac15ad4735a13a48bcad6974b
Tanzil Pasha <84717048+tanzil7786@users.noreply.github.com>
2023-05-14 14:09:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // complete the given function
 
 function palindrome(str){
-
+	let isPalindrome=true;
+let i=0;j=str.length-1;
+	while(i<=j){
+		if(str[i]!=str[j]){
+			isPalindrome=false;
+		}
+		i++;
+		j--;
+	}
+	return isPalindrome;
 }
 module.exports = palindrome
