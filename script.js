b6285f2d03f642d4f4e4e05a7da87d32a52a4c6f
Tanzil Pasha <84717048+tanzil7786@users.noreply.github.com>
2023-05-27 12:08:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,9 @@
 function palindrome(str){
 	let isPalindrome=true;
 let i=0;j=str.length-1;
-	while(i<=j){
-		if(str[i]!=str[j]){
+	while(i<=j && isPalindrome){
+		if(str[i]!==str[j]){
 			isPalindrome=false;
-			break;
 		}
 		i++;
 		j--;
