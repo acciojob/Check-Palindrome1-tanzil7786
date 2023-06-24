98633f40364181585f913ce623cd6d8012d7acc2
Tanzil Pasha <84717048+tanzil7786@users.noreply.github.com>
2023-06-24 16:44:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 function palindrome(str){
 	let isPalindrome=str.replace(/[^a-zA-Z0-9]/g,'').toLowerCase();
-	let len=isPalindrome.length;
+	let length=isPalindrome.length;
 	for(let i=0;i<Math.floor(length/2);i++){
 		if(isPalindrome[i]!==isPalindrome[length-1-i]){
 			return false;
